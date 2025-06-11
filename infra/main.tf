8d50093d842ef96bbd1d31af93a088189a1641c8
brunosprotte <62630549+brunosprotte@users.noreply.github.com>
2025-06-11 18:59:32 -0300

    adiciona tabela de mensagens

--- infra/main.tf
+++ infra/main.tf
@@ -289,6 +289,23 @@ resource "aws_dynamodb_table" "contatos" {
   }
 }
 
+resource "aws_dynamodb_table" "mensagens_pendentes" {
+  name         = "mensagens_pendentes"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "destinatario_email"
+  range_key    = "timestamp"
+
+  attribute {
+    name = "destinatario_email"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+}
+
 // S3 Bucket
 
 resource "aws_iam_policy" "s3_access" {
